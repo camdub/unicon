d9a00b30ff7e817cce48d179cd7a91bf5bd421e7
Cameron Woodmansee <cameron29w@gmail.com>
2018-09-18 12:34:15 -0400

    Remove Figma dependency

--- packages/unicon/src/index.js
+++ packages/unicon/src/index.js
@@ -1,17 +1,9 @@
-const {
-  setFigmaToken,
-  getSvgsFromFigma,
-  watchFigmaFile,
-} = require('./get-svgs-from-figma')
 const { getSvgsFromSketch, watchSketchFile } = require('./get-svgs-from-sketch')
 const { getSvgsFromFolder, watchFolder } = require('./get-svgs-from-folder')
 
 module.exports = {
-  setFigmaToken,
-  getSvgsFromFigma,
   getSvgsFromSketch,
   getSvgsFromFolder,
-  watchFigmaFile,
   watchSketchFile,
   watchFolder,
 }
